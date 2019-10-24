b7cc4f78103c6ac7c5c7317423366271bd867e10
Akashah Ahamad <akashah@2enapps.my>
2019-10-24 16:17:03 +0800

    bugfix service provider

--- src/providers/CoreProvider.php
+++ src/providers/CoreProvider.php
@@ -8,10 +8,10 @@ class CoreProvider extends ServiceProvider{
 	
 	public function boot(){
 
-		$this->loadRoutesFrom(__DIR__.'/routes/web.php');
-		$this->loadViewsFrom(__DIR__.'/views', 'packagetemplate');
-		$this->loadMigrationsFrom(__DIR__.'/migrations');
-		$this->loadTranslationsFrom(__DIR__.'/lang', 'packagetemplate');
+		$this->loadRoutesFrom(__DIR__.'../routes/web.php');
+		$this->loadViewsFrom(__DIR__.'../views', 'packagetemplate');
+		$this->loadMigrationsFrom(__DIR__.'../migrations');
+		$this->loadTranslationsFrom(__DIR__.'../lang', 'packagetemplate');
 
 	}
 
